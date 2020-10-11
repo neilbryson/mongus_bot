06f45e0d66dd2b61287be7fa1481fbfefd2c94de
Neil Bryson Cargamento <neilbrysonmc@gmail.com>
2020-10-11 09:58:36 +0800

    add EmbedUtilities
    Signed-off-by: Neil Bryson Cargamento <neilbrysonmc@gmail.com>

---
+++ Utilities/EmbedUtilities.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.Text;
+using Discord;
+
+namespace mongus_bot.Utilities
+{
+    class EmbedUtilities
+    {
+        public static Embed BuildEmbed(string title, string description, string footer, Color color)
+            => new EmbedBuilder().WithTitle(title)
+                .WithDescription(description)
+                .WithFooter(f => f.Text = footer)
+                .WithColor(color)
+                .WithCurrentTimestamp()
+                .Build();
+
+        public static string CreateList(List<string> list)
+        {
+            var sb = new StringBuilder();
+            list.ForEach(l =>
+            {
+                sb.Append($"- {l}\n");
+            });
+            return sb.ToString();
+        }
+    }
+}
